a6cd7bf496ebfc275692204b3ce38a0869ee0d13
Genenu <gackmar@outlook.com>
2025-10-28 11:22:52 +0100

    contact form independent route integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Success from "./success";
+import ContactPage from "./ContactPage";
 
 const router = createBrowserRouter ( createRoutesFromElements (
   <>
     <Route path="/" element={<Layout />} />
     <Route path="/success" element={<Success />} />
+    <Route path="/contact" element={<ContactPage />} />
   </>
 ) )
 
